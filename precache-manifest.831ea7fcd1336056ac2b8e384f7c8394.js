4b6c5c5ed6d9e962d535523acafb47ec3c5d8cf8
evelint <myuuuu@gmail.com>
2020-08-18 15:52:43 +0800

    Updates

---
+++ precache-manifest.831ea7fcd1336056ac2b8e384f7c8394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92b11dd81e90b72e385261a18121fe9",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "50a9b072724301b5f9b3",
+    "url": "/hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/hello-world/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-world/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a9b072724301b5f9b3",
+    "url": "/hello-world/static/js/main.bd0dccc5.chunk.js"
+  },
+  {
+    "revision": "2800c7a7f8094c027159",
+    "url": "/hello-world/static/js/runtime-main.e32c7542.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
